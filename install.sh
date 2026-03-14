dde814d2dd48734d088d983d0acdc636b69b4eb0
Aniket-89 <aniketkumarg99@gmail.com>
2026-03-14 19:09:14 +0530

    removed chmod +x steps because now using sudo bash command to run the scripts. closes #35

--- install.sh
+++ install.sh
@@ -6,11 +6,6 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "--- Running Setup Scripts ---"
 
-# Ensure scripts are executable
-chmod +x "$SCRIPT_DIR/scripts/wifi-start.sh"
-chmod +x "$SCRIPT_DIR/scripts/setup_mavlink.sh"
-chmod +x "$SCRIPT_DIR/scripts/setup_camera.sh"
-
 # Run them
 sudo bash "$SCRIPT_DIR/scripts/wifi-start.sh"
 sudo bash "$SCRIPT_DIR/scripts/setup_mavlink.sh"

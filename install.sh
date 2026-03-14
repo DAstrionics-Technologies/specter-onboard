ef1a8c27d138c1f3929e27eff66771e651c23d1b
Aniket-89 <aniketkumarg99@gmail.com>
2026-03-14 19:43:53 +0530

    fix:exported the script dir variable in the install script and ran the config save of mavlink using sudo

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -e
 
 # Get directory of this script
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export SCRIPT_DIR
 
 echo "--- Running Setup Scripts ---"
 
--- scripts/setup_mavlink.sh
+++ scripts/setup_mavlink.sh
@@ -13,13 +13,15 @@ sudo ninja -C build install
 
 # Copy config
 sudo mkdir -p /etc/specter
+sudo mkdir -p /etc/mavlink-router
+
 sudo cp $SCRIPT_DIR/config/mavlink-router.env.template /etc/specter/mavlink-router.env
 source /etc/specter/mavlink-router.env
 
-sudo mkdir -p /etc/mavlink-router
+export GCS_IP MAVLINK_PORT MAVLINK_TCP_PORT FC_DEVICE FC_BAUD
 
-envsubst < $SCRIPT_DIR/config/mavlink-router.conf.template \
-  > /etc/mavlink-router/main.conf
+envsubst < $SCRIPT_DIR/config/mavlink-router.conf.template | \
+  sudo tee /etc/mavlink-router/main.conf > /dev/null
 
 # Install systemd service
 sudo cp $SCRIPT_DIR/systemd/mavlink-router.service /etc/systemd/system/mavlink-router.service
